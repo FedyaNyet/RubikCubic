2fd1c9cb6c4f7184b550eb08c5cfea89250d241f
fedyanyet <fyodor.wolf@gmail.com>
2014-11-30 15:10:29 -0500

    merged previous commits.

--- src/CubeSolver.java
+++ src/CubeSolver.java
@@ -1,66 +1,115 @@
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 import FastCopy.DeepCopy;
 
 public class CubeSolver {
 
-	static final int MAX_DEPTH = 20;
+	static final int MAX_DEPTH = 10;
 	
-	int activeDepth;
+	String algorithm;
+	
 	CubeNode activeNode;
-	CubeNode root;
+	LinkedList<CubeNode> queue;
 	
 	CubeSolver(Cube cube){
-		this.activeDepth = 0;
-		this.root = new CubeNode(cube);
-		this.activeNode = this.root;
+		initWithCube(cube);
 	}
 	
-	public boolean solveBFS(){
-		while(!activeNode.hasSolvedChild()){
-			int numChildren = this.activeNode.children.size();
-			this.activeNode = this.activeNode.children.get(numChildren-1);
-			this.activeDepth++;
-			if(activeDepth == MAX_DEPTH){
-				this.activeNode.children.remove(this.activeNode.children.size()-1);
+	CubeSolver(Cube cube, String algorithm){
+		initWithCube(cube);
+		this.algorithm = algorithm;
+	}
+	
+	private void initWithCube(Cube cube){
+		this.activeNode =  new CubeNode(cube);
+		queue = new LinkedList<CubeNode>();
+		queue.add(this.activeNode);
+		this.algorithm = "BFS";
+	}
+
+	private void stepSolveBFS(){
+		activeNode = queue.pop();
+		if(activeNode.cube.isSolved())
+			return;
+		if( activeNode.depth < CubeSolver.MAX_DEPTH){
+			activeNode.defineChildren();
+			Iterator<CubeNode> entries = activeNode.children.values().iterator();
+			while (entries.hasNext()) {
+				queue.add(entries.next());
 			}
 		}
-		return false;
 	}
 	
+	private void stepSolveDFS(){
+		if(activeNode.cube.isSolved())
+			return;
+		if(activeNode.depth >= CubeSolver.MAX_DEPTH){
+			activeNode.parent.children.remove(activeNode.move);
+			activeNode = activeNode.parent;
+			return;
+		}
+		if(activeNode.children == null){
+			activeNode.defineChildren();
+		}
+		Iterator<CubeNode> entries = activeNode.children.values().iterator();
+		if(!entries.hasNext()){
+			activeNode.parent.children.remove(activeNode.move);
+			activeNode = activeNode.parent;
+			return;
+		}else{
+			activeNode = entries.next();
+			return;
+		}
+	}
 	
+	public void solve(){
+		int step = 0;
+		while(!activeNode.cube.isSolved()){
+			if(this.algorithm == "BFS"){
+				stepSolveBFS();
+			}else if(this.algorithm == "DFS"){
+				stepSolveDFS();
+			}
+		}
+	}
+
+
 	class CubeNode{
 		
-		CubeNode parent;
 		Cube cube;
-		ArrayList<CubeNode> children;
+		
+		String breadcrumbs;
+		String move;
+		int depth;
+		CubeNode parent;
+		HashMap<String, CubeNode> children;
 		
 		CubeNode(Cube cube){
 			this.cube = cube;
+			this.breadcrumbs = "";
+			depth = 0;
 		}
 		
-		void setParent(CubeNode parent){
-			this.parent = parent; 
+		CubeNode(CubeNode parent, String nextMove){
+//			System.out.println("CHILD MOVE: "+nextMove+" \n"+childCube);
+			this.cube = (Cube) DeepCopy.copy(parent.cube);
+			this.cube.doMove(nextMove);
+			this.move = nextMove;
+			this.breadcrumbs = parent.breadcrumbs + nextMove + ",";
+			this.parent = parent;
+			this.depth = parent.depth + 1;
 		}
 		
-		Boolean hasSolvedChild(){
-			if(this.children == null){
-				this.exploreChildren();
-			}
-			for(CubeNode child : this.children){
-				if (child.cube.isSolved()) return true;
-			}
-			return false;
-		}
-		
-		void exploreChildren(){
-			this.children = new ArrayList<CubeNode>();
-			for(int faceIndex=0; faceIndex<Cube.FACE_ORDER.length(); faceIndex++){
-				Cube childCube = (Cube) DeepCopy.copy(cube);
-				String nextMove = Cube.FACE_ORDER.charAt(faceIndex)+"";
-				childCube.doMove(nextMove);
-				this.children.add(new CubeNode(childCube));
+		void defineChildren(){
+			this.children = new HashMap<String, CubeNode>();
+			for(String nextMove : Cube.POSSIBLE_MOVES){
+				if (this.move != null && nextMove.charAt(0) == this.move.charAt(0))
+					continue;
+				CubeNode childNode = new CubeNode(this, nextMove);
+				this.children.put(nextMove, childNode);
 			}
 		}
 		
