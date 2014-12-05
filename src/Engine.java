93519b5b9279ea0ac074d52708f1f14cff1d039c
Fyodor Wolf <fwolf@bu.edu>
2014-12-05 10:42:04 -0500

    clearned up running scrip.

--- src/Engine.java
+++ src/Engine.java
@@ -1,19 +1,26 @@
 
 public class Engine {
-
 	
 	public static void main(String[] args){
+		
+		int sequenceLength = 5;
+		int algorith;
+//		algorith = CubeSolver.ALGORITHM_BFS;
+//		algorith = CubeSolver.ALGORITHM_DFS;
+//		algorith = CubeSolver.ALGORITHM_HUMAN;
+		algorith = CubeSolver.ALGORITHM_GENETIC;
+		
 		Cube cube = new Cube();
 		String scrambleSequence = "";
 	
 		while(cube.score()>0){
 			cube = new Cube();
-			scrambleSequence = Cube.generateMoveSequence(5);
+			scrambleSequence = Cube.generateMoveSequence(sequenceLength);
 			cube.doMoves(scrambleSequence);
 		}
 		
 		System.out.println("Scramble: "+scrambleSequence);
-		CubeSolver solver = new CubeSolver(cube, CubeSolver.ALGORITHM_GENETIC);
+		CubeSolver solver = new CubeSolver(cube, algorith);
 		
 		long startTime = System.nanoTime();
 		solver.solve();
